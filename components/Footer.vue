610240e71dc0c6bfd1099689f05cf3062e8b4a55
reginald-ai <gellishgarnerp@gmail.com>
2021-10-28 10:24:43 +0800

    🐔 done with the author

--- components/Footer.vue
+++ components/Footer.vue
@@ -101,13 +101,13 @@
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p><i class="fas fa-home me-3"></i> Cebu City,Cebu Philippines</p>
             <p>
               <i class="fas fa-envelope me-3"></i>
-              info@example.com
+              regmangubat@outlook.com
             </p>
-            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-phone me-3"></i> 63+ 961 054 5296</p>
+            <p><i class="fas fa-print me-3"></i> 63+ 961 054 5296</p>
           </div>
           <!-- Grid column -->
         </div>
---
+++ pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 c>HEllo</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+h1 {
+  color: black;
+}
+</style>
